aef610e6ddedfde41328c3be8f2947e70e9b2e68
bthank <bthankachan@yahoo.com>
2021-11-20 21:17:41 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = "";
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onButtonClick() {
+    console.log('Button was clicked     this.includeLetters=', this.includeLetters);
+    this.password = "My Password";
+    console.log(`
+      About to generate password:
+        - Include letters: ${this.includeLetters}
+        - Include numbers: ${this.includeNumbers}
+        - Include symbols: ${this.includeSymbols}
+    `);
+  }
+
+  onLengthChange() {
+    console.log('In onLengthChange    change was detected');
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+    console.log('In onChangeUseLetters -- a change event was detected   this.includeLetters=', this.includeLetters); 
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+    console.log('In onChangeUseNumbers -- a change event was detected   this.includeNumbers=', this.includeNumbers); 
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+    console.log('In onChangeUseSymbols -- a change event was detected   this.includeSymbols=', this.includeSymbols); 
+  }
+
 }
