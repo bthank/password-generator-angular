26b34ad2c97b6005123747a344008d68f8b56e36
bthank <bthankachan@yahoo.com>
2021-11-20 21:55:19 -0500

    Added password generation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
   password = "";
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
 
   onButtonClick() {
-    console.log('Button was clicked     this.includeLetters=', this.includeLetters);
-    this.password = "My Password";
+    console.log('Button was clicked');
+    this.password = "";
+
+    const numbers = "0123456789";
+    const letters ="abcdefghijklmnopqrstuvwxyz";
+    const symbols = "!@#$%^&*()_+";
+
+    let validChars = "";
+    if (this.includeLetters) {
+      validChars += letters;
+    } 
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i=0; i < this.length; i++){
+      const randomIndex = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[randomIndex];
+      console.log("In generatedPassword for loop    randomIndex=",randomIndex,"   this.password=",this.password)
+    }
+    this.password = generatedPassword;
+
     console.log(`
       About to generate password:
         - Include letters: ${this.includeLetters}
         - Include numbers: ${this.includeNumbers}
         - Include symbols: ${this.includeSymbols}
+        - validChars: ${validChars}
+        - generatedPassword: ${generatedPassword}
     `);
   }
 
-  onLengthChange() {
-    console.log('In onLengthChange    change was detected');
+  onLengthChange(event: any) {
+    const parsedValue = parseInt(event.target.value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    } else {
+      this.length = 0;
+    }
+    console.log('In onLengthChange    change was detected    this.length=',this.length);
   }
 
   onChangeUseLetters() {
